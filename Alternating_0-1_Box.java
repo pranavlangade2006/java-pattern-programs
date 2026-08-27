0ffe413cae3103e21860de96da461c137ad84e76
pranav langade <pranavslangade@gmail.com>
2026-08-27 22:59:02 +0530

    Added Alternating 0-1 Box Pattern

---
+++ Alternating_0-1_Box.java
@@ -0,0 +1,17 @@
+class demo{
+	public static void main(String[]args){
+		int n=5;
+	for(int i=1 ; i<=n ;i++){
+		for(int j=1 ; j<=n ; j++){
+	int k = i+j;
+		if(k%2==0){
+	System.out.print("0");
+		}
+	else{
+	System.out.print("1");
+	}
+		}
+	System.out.println();
+	}
+		}
+			}
